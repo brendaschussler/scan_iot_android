26b70ed4496f26f0e49e8e768532984e395aa816
brendaschussler <brendaschussler@gmail.com>
2025-05-28 14:43:47 -0400

    After changing register screen and institutional email verification

--- app/src/main/java/com/example/scaniot/model/User.kt
+++ app/src/main/java/com/example/scaniot/model/User.kt
@@ -3,5 +3,10 @@ package com.example.scaniot.model
 data class User (
     var userId: String,
     var name: String,
-    var email: String
+    var lastName: String,
+    var country: String,
+    var email: String,
+    var organization: String? = null,
+    var jobTitle: String? = null,
+    var manager: String? = null
 )
